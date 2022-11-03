8abecb83154e3b0fbfa1c2781780a001676f695f
Jinyeong <jinyeongpark1205@gamil.com>
2022-11-03 11:33:47 -0700

    create Car object and draw

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ const canvas = document.getElementById("myCanvas");
 canvas.height = window.innerHeight;
 canvas.width = 200;
 
+const ctx = canvas.getContex("2d");
+const car = new Car(100, 100, 30, 50);
+car.draw(ctx);
