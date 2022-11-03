0f4e57debba20d16f4a5e4a5fe658a474692dd15
Jinyeong <jinyeongpark1205@gamil.com>
2022-11-03 11:56:11 -0700

    add car move control with keyboard

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const canvas = document.getElementById("myCanvas");
 canvas.height = window.innerHeight;
 canvas.width = 200;
 
-const ctx = canvas.getContex("2d");
+const ctx = canvas.getContext("2d");
 const car = new Car(100, 100, 30, 50);
 car.draw(ctx);
